135beb55d74b1eb18e698f9018c58e3c741c490a
Samir Jadhav <samirzjadhav@gmail.com>
2023-04-14 21:27:12 +0530

    feat: added scrpt and styled it

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const apiKey = "c828bece46c9a82fad997ccd2ec60322";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=nagpur";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   let data = await response.json();
   console.log(data);
 
@@ -14,5 +18,24 @@ async function checkWeather() {
   document.querySelector(".visibility").innerHTML =
     data.visibility / 1000 + " Km";
   document.querySelector(".feels").innerHTML = data.main.feels_like + "°C";
+
+  console.log(data.weather);
+
+  if (data.weather[0].main == "Clouds") {
+    weatherIcon.src = "images/clouds.png";
+  } else if (data.weather[0].main == "Clear") {
+    weatherIcon.src = "./images/clear.png";
+  } else if (data.weather[0].main == "Drizzle") {
+    weatherIcon.src = "./images/drizzle.png";
+  } else if (data.weather[0].main == "Mist") {
+    weatherIcon.src = "./images/mist.png";
+  } else if (data.weather[0].main == "Rain") {
+    weatherIcon.src = "./images/rain.png";
+  } else if (data.weather[0].main == "Snow") {
+    weatherIcon.src = "./images/snow.png";
+  }
 }
-checkWeather();
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
